4c3c02702c9e1973728919fdeb346e1590a3f694
madhu-webkorps <madhu@webkorps.com>
2022-09-20 19:08:11 +0530

    association

--- app/models/product_field.rb
+++ app/models/product_field.rb
@@ -1,3 +1,4 @@
 class ProductField < ApplicationRecord
-    
+
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+#association with product fields
+  has_many :product_fields
+  
 end
