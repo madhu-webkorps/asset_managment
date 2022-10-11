1a7b2c91fc692cb632e1a6e15073feae55709710
madhu-webkorps <madhu@webkorps.com>
2022-10-11 11:39:13 +0530

    sothing done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,17 @@
 class Product
 
-
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
   db = Mongoid::default_client
   collection = db[:collection_name]
 
+ #associaiton with user
   belongs_to :user
   
+  # custom method for insert record in record
   def Product.product_with_user(id , product_params)
-    debugger
-     Product.collection.insert_one(user_id: id , params: product_params)  
-     
+     Product.collection.insert_one(user_id: id  , params: product_params, created_at: Time.now ,expiredate: Time.now + 2.years)
   end
 
 end
